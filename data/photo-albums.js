b0f435566761a8b35f9afefde59e02191743bac3
Oleg <oleg3479881328@gmail.com>
2026-05-04 20:02:02 -0400

    Add Elena Podkovtseva photo album

--- data/photo-albums.js
+++ data/photo-albums.js
@@ -54,4 +54,59 @@ window.PHOTO_ALBUMS = [
       },
     ],
   },
+  {
+    personId: "elena-podkovtseva",
+    title: "Елена Николаевна Подковцева",
+    description: "Фотографии из семейного альбома.",
+    photos: [
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20200819-WA0005.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 1",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20220513-WA0000.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 2",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20220708-WA0004.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 3",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20220711-WA0003.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 4",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20230818-WA0000.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 5",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20231225-WA0016.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 6",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20241107-WA0001.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 7",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20241109-WA0001.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 8",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20241109-WA0002.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 9",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20250724-WA0002.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 10",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20251225-WA0003.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 11",
+      },
+      {
+        src: "assets/photo-albums/elena-podkovtseva/Photos-3-001 (6)/IMG-20251225-WA0009.jpg",
+        caption: "Фотоальбом Елены Николаевны. Фото 12",
+      },
+    ],
+  },
 ];
