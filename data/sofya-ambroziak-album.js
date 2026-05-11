c76dc0e005d60d4a4f5fa7f084b72e6d53786fed
oleg3479881328-code <oleg3479881328@gmail.com>
2026-05-11 14:03:36 -0400

    Add Sofya Ambroziak photo album data

---
+++ data/sofya-ambroziak-album.js
@@ -0,0 +1,18 @@
+window.PHOTO_ALBUMS = window.PHOTO_ALBUMS || [];
+
+window.PHOTO_ALBUMS.push({
+  description: "Фотографии из семейного альбома.",
+  personId: "sofya-ambroziak",
+  photos: [
+    {
+      caption: "Фотоальбом Софьи Владимировны. Фото 1",
+      src: "assets/photo-albums/IMG-20260511-WA0025.jpg",
+    },
+    {
+      caption: "Фотоальбом Софьи Владимировны. Фото 2",
+      src: "assets/photo-albums/IMG-20260511-WA0022.jpg",
+    },
+  ],
+  portrait: "assets/photo-albums/IMG-20260511-WA0025.jpg",
+  title: "Софья Владимировна Амброзиак",
+});
