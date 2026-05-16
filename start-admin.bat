9e0188cbb15aaf8826bd7e02936341185a4a8102
Oleg <oleg3479881328@gmail.com>
2026-05-15 22:44:24 -0400

    Publish site updates

--- start-admin.bat
+++ start-admin.bat
@@ -3,5 +3,6 @@ cd /d "%~dp0"
 for /f "tokens=5" %%p in ('netstat -ano ^| findstr /r /c:":8765 .*LISTENING"') do (
   taskkill /PID %%p /F >nul 2>nul
 )
+start "" http://127.0.0.1:8765/
 start "" http://127.0.0.1:8765/admin/
 python tools\admin_server.py
