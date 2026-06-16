70a750ee05e4612e3bed543dc9d8054cc471f1b4
oleg3479881328-code <oleg3479881328@gmail.com>
2026-06-16 18:43:43 -0400

    Add EN-US family translation pack

---
+++ data/i18n/family.en-US.js
@@ -0,0 +1,269 @@
+window.FAMILY_EN_US = {
+  transliterationStandard: {
+    note: "Use one consistent US-facing transliteration layer. Preserve original Cyrillic names in details.",
+    choices: {
+      petr: "Petr",
+      nikolay: "Nikolay",
+      yuriy: "Yuriy",
+      sergey: "Sergey",
+      dmitry: "Dmitry",
+      natalya: "Natalya",
+      sofya: "Sofya",
+      valerievna: "Valerievna"
+    }
+  },
+  relationshipWording: {
+    default: "Translate only what the Russian source states. Do not infer legal status.",
+    currentSpouse: "Spouse/partner recorded with {name}.",
+    formerSpouse: "Former spouse of {name}.",
+    parent: "Parent of {name}.",
+    child: "Child of {name}."
+  },
+  people: {
+    "alexander-fateev": {
+      displayName: "Alexander Konstantinovich Fateev / Александр Константинович Фатеев",
+      shortName: "Alexander Fateev",
+      notes: ["Spouse/partner recorded with Alexandra Dmitrievna Kureneva."]
+    },
+    "alexander-tankov": {
+      displayName: "Alexander Nikolaevich Tankov / Александр Николаевич Таньков",
+      shortName: "Alexander Tankov",
+      notes: ["Father of Olga Alexandrovna Tankova and Natalya Alexandrovna Tankova."]
+    },
+    "alexandra-kureneva": {
+      displayName: "Alexandra Dmitrievna Kureneva / Александра Дмитриевна Куренева",
+      shortName: "Alexandra Kureneva",
+      notes: ["Spouse/partner recorded with Alexander Konstantinovich Fateev."]
+    },
+    "alexandra-pirina": {
+      displayName: "Alexandra Sergeevna Pirina / Александра Сергеевна Пирина",
+      shortName: "Alexandra Pirina",
+      notes: ["Spouse/partner recorded with Oleg Igorevich Povalyukhin."]
+    },
+    "anatoly-povalyukhin": {
+      displayName: "Anatoly Igorevich Povalyukhin / Анатолий Игоревич Повалюхин",
+      shortName: "Anatoly Povalyukhin",
+      notes: ["Son of Tamara Nikolaevna Povalyukhina and Igor Petrovich Povalyukhin."]
+    },
+    "anna-fateeva": {
+      displayName: "Anna Konstantinovna Fateeva / Анна Константиновна Фатеева",
+      shortName: "Anna Fateeva",
+      notes: ["Daughter of Konstantin Alexandrovich Fateev and Tamara Antonovna Zhuleva."]
+    },
+    "anna-kuteinikova": {
+      displayName: "Anna Sergeevna Kuteinikova / Анна Сергеевна Кутейникова",
+      shortName: "Anna Kuteinikova",
+      notes: ["Daughter of Sergey Ivanovich Kuteinikov and Maria Kuteinikova.", "Spouse/partner recorded with Dmitry Andreevich Kurenev."]
+    },
+    "anna-povalyukhina": {
+      displayName: "Anna Olegovna Povalyukhina / Анна Олеговна Повалюхина",
+      shortName: "Anna Povalyukhina",
+      notes: ["Daughter of Oleg Igorevich Povalyukhin and Alexandra Sergeevna Pirina."]
+    },
+    "daniil-fateev": {
+      displayName: "Daniil Fateev / Даниил Фатеев",
+      shortName: "Daniil Fateev",
+      notes: ["Son of Nikita Pavlovich Fateev and Daryana Sergeevna Medvedeva."]
+    },
+    "daryana-medvedeva": {
+      displayName: "Daryana Sergeevna Medvedeva / Дарьяна Сергеевна Медведева",
+      shortName: "Daryana Medvedeva",
+      notes: ["Spouse/partner recorded with Nikita Pavlovich Fateev."]
+    },
+    "dmitry-dmitrievich-solovyov": {
+      displayName: "Dmitry Dmitrievich Solovyov / Дмитрий Дмитриевич Соловьёв",
+      shortName: "Dmitry Solovyov",
+      notes: ["Son of Dmitry Ivanovich Solovyov and Valentina Andreevna Solovyova.", "Brother of Vera Dmitrievna Solovyova."]
+    },
+    "dmitry-kurenev": {
+      displayName: "Dmitry Andreevich Kurenev / Дмитрий Андреевич Куренёв",
+      shortName: "Dmitry Kurenev",
+      notes: ["Spouse/partner recorded with Anna Sergeevna Kuteinikova."]
+    },
+    "dmitry-solovyov": {
+      displayName: "Dmitry Ivanovich Solovyov / Дмитрий Иванович Соловьёв",
+      shortName: "Dmitry Solovyov",
+      notes: ["Father of Vera Dmitrievna Solovyova.", "Father of Dmitry Dmitrievich Solovyov."]
+    },
+    "elena-podkovtseva": {
+      displayName: "Elena Nikolaevna Podkovtseva / Елена Николаевна Подковцева",
+      shortName: "Elena Podkovtseva",
+      notes: ["Daughter of Nikolay Pavlovich Podkovtsev and Vera Dmitrievna Kureneva."]
+    },
+    "elizaveta-kureneva": {
+      displayName: "Elizaveta Dmitrievna Kureneva / Елизавета Дмитриевна Куренёва",
+      shortName: "Elizaveta Kureneva",
+      notes: ["Daughter of Anna Sergeevna Kuteinikova and Dmitry Andreevich Kurenev."]
+    },
+    "gerard-hurley": {
+      displayName: "Gerard Hurley / Джерард Хёрли",
+      shortName: "Gerard Hurley",
+      notes: ["Spouse/partner recorded with Natalya Olegovna Povalyukhina."]
+    },
+    "hawkin-hurley": {
+      displayName: "Hawkin Randall Hurley / Хокин Рэндал Хёрли",
+      shortName: "Hawkin Hurley",
+      notes: ["Son of Natalya Olegovna Povalyukhina and Gerard Hurley."]
+    },
+    "igor-grigoryev": {
+      displayName: "Igor Nikolaevich Grigoryev / Игорь Николаевич Григорьев",
+      shortName: "Igor Grigoryev",
+      notes: ["Son of Nikolay Dmitrievich Grigoryev and Natalya Dmitrievna Kureneva."]
+    },
+    "igor-povalyukhin": {
+      displayName: "Igor Petrovich Povalyukhin / Игорь Петрович Повалюхин",
+      shortName: "Igor Povalyukhin",
+      notes: ["Spouse/partner recorded with Tamara Nikolaevna Povalyukhina."]
+    },
+    "konstantin-fateev": {
+      displayName: "Konstantin Alexandrovich Fateev / Константин Александрович Фатеев",
+      shortName: "Konstantin Fateev",
+      notes: ["Spouse/partner recorded with Tamara Antonovna Zhuleva."]
+    },
+    "maria-kuteinikova": {
+      displayName: "Maria Kuteinikova / Мария Кутейникова",
+      shortName: "Maria Kuteinikova",
+      notes: ["Mother of Anna Sergeevna Kuteinikova."]
+    },
+    "maria-povalyukhina": {
+      displayName: "Maria Olegovna Povalyukhina / Мария Олеговна Повалюхина",
+      shortName: "Maria Povalyukhina",
+      notes: ["Daughter of Oleg Igorevich Povalyukhin and Olga Alexandrovna Tankova."]
+    },
+    "marina-drozdova": {
+      displayName: "Marina Valerievna Drozdova / Марина Валерьевна Дроздова",
+      shortName: "Marina Drozdova",
+      notes: ["Spouse/partner recorded with Pavel Alexandrovich Fateev."]
+    },
+    "natalya-kureneva": {
+      displayName: "Natalya Dmitrievna Kureneva / Наталья Дмитриевна Куренёва",
+      shortName: "Natalya Kureneva",
+      notes: ["Spouse/partner recorded with Nikolay Dmitrievich Grigoryev."]
+    },
+    "natalya-povalyukhina": {
+      displayName: "Natalya Olegovna Povalyukhina / Наталья Олеговна Повалюхина",
+      shortName: "Natalya Povalyukhina",
+      notes: ["Spouse/partner recorded with Gerard Hurley."]
+    },
+    "natalya-pirina": {
+      displayName: "Natalya Nikolaevna Pirina / Наталья Николаевна Пирина",
+      shortName: "Natalya Pirina",
+      notes: ["Mother of Alexandra Sergeevna Pirina."]
+    },
+    "natalya-tankova": {
+      displayName: "Natalya Alexandrovna Tankova / Наталья Александровна Танькова",
+      shortName: "Natalya Tankova",
+      notes: ["Daughter of Alexander Nikolaevich Tankov and Nina Ivanovna Tankova.", "Sister of Olga Alexandrovna Tankova."]
+    },
+    "nikita-fateev": {
+      displayName: "Nikita Pavlovich Fateev / Никита Павлович Фатеев",
+      shortName: "Nikita Fateev",
+      notes: ["Spouse/partner recorded with Daryana Sergeevna Medvedeva."]
+    },
+    "nikolay-grigoryev": {
+      displayName: "Nikolay Dmitrievich Grigoryev / Николай Дмитриевич Григорьев",
+      shortName: "Nikolay Grigoryev",
+      notes: ["Spouse/partner recorded with Natalya Dmitrievna Kureneva."]
+    },
+    "nikolay-podkovtsev": {
+      displayName: "Nikolay Pavlovich Podkovtsev / Николай Павлович Подковцев",
+      shortName: "Nikolay Podkovtsev",
+      notes: ["Spouse/partner recorded with Vera Dmitrievna Kureneva."]
+    },
+    "nina-tankova": {
+      displayName: "Nina Ivanovna Kalinina / Нина Ивановна Калинина",
+      shortName: "Nina Kalinina",
+      notes: ["Mother of Olga Alexandrovna Tankova and Natalya Alexandrovna Tankova."]
+    },
+    "oleg-povalyukhin": {
+      displayName: "Oleg Igorevich Povalyukhin / Олег Игоревич Повалюхин",
+      shortName: "Oleg Povalyukhin",
+      notes: ["Son of Tamara Nikolaevna Povalyukhina and Igor Petrovich Povalyukhin.", "Spouse/partner recorded with Vera Dmitrievna Solovyova, Alexandra Sergeevna Pirina, and Olga Alexandrovna Tankova."]
+    },
+    "olga-tankova": {
+      displayName: "Olga Alexandrovna Tankova / Ольга Александровна Танькова",
+      shortName: "Olga Tankova",
+      notes: ["Spouse/partner recorded with Oleg Igorevich Povalyukhin.", "Daughter of Alexander Nikolaevich Tankov and Nina Ivanovna Tankova."]
+    },
+    "pavel-fateev": {
+      displayName: "Pavel Alexandrovich Fateev / Павел Александрович Фатеев",
+      shortName: "Pavel Fateev",
+      notes: ["Spouse/partner recorded with Marina Valerievna Drozdova.", "Spouse/partner recorded with Vera Nikolaevna Plitkina."]
+    },
+    "polina-musienko": {
+      displayName: "Polina Yurievna Musienko / Полина Юрьевна Мусиенко",
+      shortName: "Polina Musienko",
+      notes: ["Daughter of Alexandra Sergeevna Pirina.", "Sister of Anna Olegovna Povalyukhina."]
+    },
+    "petr-povalyukhin": {
+      displayName: "Petr Petrovich Povalyukhin / Пётр Петрович Повалюхин",
+      shortName: "Petr Povalyukhin",
+      notes: ["Father of Igor Petrovich Povalyukhin."]
+    },
+    "sasha-fateev": {
+      displayName: "Alexander Pavlovich Fateev / Александр Павлович Фатеев",
+      shortName: "Alexander Fateev",
+      notes: ["Child of Pavel Alexandrovich Fateev and Vera Nikolaevna Plitkina."]
+    },
+    "sergey-pirin": {
+      displayName: "Sergey Ivanovich Pirin / Сергей Иванович Пирин",
+      shortName: "Sergey Pirin",
+      notes: ["Father of Alexandra Sergeevna Pirina."]
+    },
+    "sergey-kuteinikov": {
+      displayName: "Sergey Ivanovich Kuteinikov / Сергей Иванович Кутейников",
+      shortName: "Sergey Kuteinikov",
+      notes: ["Father of Anna Sergeevna Kuteinikova."]
+    },
+    "sofya-ambroziak": {
+      displayName: "Sofya Vladimirovna Ambroziak / Софья Владимировна Амброзиак",
+      shortName: "Sofya Ambroziak",
+      notes: ["Daughter of Vera Dmitrievna Solovyova and Vladimir Ambroziak."]
+    },
+    "tamara-grigoryeva": {
+      displayName: "Tamara Nikolaevna Grigoryeva / Povalyukhina / Тамара Николаевна Григорьева / Повалюхина",
+      shortName: "Tamara Povalyukhina",
+      notes: ["Spouse/partner recorded with Igor Petrovich Povalyukhin."]
+    },
+    "tamara-zhuleva": {
+      displayName: "Tamara Antonovna Zhuleva / Тамара Антоновна Жулёва",
+      shortName: "Tamara Zhuleva",
+      notes: ["Spouse/partner recorded with Konstantin Alexandrovich Fateev."]
+    },
+    "valentina-solovyova": {
+      displayName: "Valentina Andreevna Solovyova / Валентина Андреевна Соловьёва",
+      shortName: "Valentina Solovyova",
+      notes: ["Mother of Vera Dmitrievna Solovyova.", "Mother of Dmitry Dmitrievich Solovyov."]
+    },
+    "vera-kureneva": {
+      displayName: "Vera Dmitrievna Kureneva / Вера Дмитриевна Куренева",
+      shortName: "Vera Kureneva",
+      notes: ["Spouse/partner recorded with Nikolay Pavlovich Podkovtsev."]
+    },
+    "vera-plitkina": {
+      displayName: "Vera Nikolaevna Plitkina / Вера Николаевна Плиткина",
+      shortName: "Vera Plitkina",
+      notes: ["Spouse/partner recorded with Pavel Alexandrovich Fateev."]
+    },
+    "vera-povalyukhina": {
+      displayName: "Vera Vasilievna Povalyukhina (Gedaspova) / Вера Васильевна Повалюхина (Гедаспова)",
+      shortName: "Vera Povalyukhina",
+      notes: ["Spouse/partner recorded with Petr Petrovich Povalyukhin.", "Mother of Igor Petrovich Povalyukhin."]
+    },
+    "vera-solovyova": {
+      displayName: "Vera Dmitrievna Solovyova / Вера Дмитриевна Соловьёва",
+      shortName: "Vera Solovyova",
+      notes: ["Former spouse of Oleg Igorevich Povalyukhin.", "Former spouse of Vladimir Ambroziak."]
+    },
+    "vladimir-ambroziak": {
+      displayName: "Vladimir Ambroziak / Владимир Амброзиак",
+      shortName: "Vladimir Ambroziak",
+      notes: ["Spouse/partner recorded with Vera Dmitrievna Solovyova.", "Father of Sofya Vladimirovna Ambroziak."]
+    },
+    "yuriy-musienko": {
+      displayName: "Yuriy Musienko / Юрий Мусиенко",
+      shortName: "Yuriy Musienko",
+      notes: ["Father of Polina Yurievna Musienko."]
+    }
+  }
+};
